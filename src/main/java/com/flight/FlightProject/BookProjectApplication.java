ffe9c7cb57b75ca9cdc05ce316dd8359972def61
Sujan.Lamichhane <sujann42@gmail.com>
2020-03-14 12:41:34 -0500

    uml diagram created

--- src/main/java/com/flight/FlightProject/BookProjectApplication.java
+++ src/main/java/com/flight/FlightProject/BookProjectApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("com.books.BookProject.Repository")
+@EnableJpaRepositories("com.flight.FlightProject.persistence.repo")
 @EntityScan("com.books.BookProject.Model")
 @ServletComponentScan
-@SpringBootApplication(scanBasePackages = "com.books")
+@SpringBootApplication(scanBasePackages = "com.flight")
 public class BookProjectApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/flight/FlightProject/persistence/Model/Flight.java
+++ src/main/java/com/flight/FlightProject/persistence/Model/Flight.java
@@ -1,16 +1,8 @@
 package com.flight.FlightProject.persistence.Model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.*;
+import java.util.Objects;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 @Entity
 public class Flight {
     @Id
@@ -20,13 +12,58 @@ public class Flight {
     @Column(nullable = false, unique = true)
     private String name;
 
+    @Column(nullable = false)
+    private String length;
+
+    public Flight(){
+
+    }
+
     public long getId() {
         return id;
     }
 
-    @Column(nullable = false)
-    private String length;
+    public void setId(long id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getLength() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return "Flight{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", length='" + length + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Flight flight = (Flight) o;
+        return id == flight.id &&
+                name.equals(flight.name) &&
+                length.equals(flight.length);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, length);
+    }
 }
